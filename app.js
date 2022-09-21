0836f0dfd2d75638d040bf0c852b04f2a72cf788
martindellavecchia <dellavec@gmail.com>
2022-09-20 21:38:06 -0300

    commit mercadolierbre

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(express.urlencoded({ extended: false }));
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {                        //levanta el servidor usando una variable de entorno O el puerto 3000
     console.log("Servidor corriendo en el puerto 3000...")
 })
 app.get("/", (req, res) => {
